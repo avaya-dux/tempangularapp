12ccf414b6e2d2cb231eb48d78495c9e9e539f8f
Joe Sebast <joe.sebast.avaya@gmail.com>
2024-02-27 14:31:01 -0600

    add base app with neo-angular implementation

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FauxButtonComponent, StaticComponent } from '@avaya/neo-angular';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [FauxButtonComponent, StaticComponent, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'tempangularapp';
+}
